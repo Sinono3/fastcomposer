5f5c2d9a798c022705566fc3f50ef07d94dee9d2
Sinono3 <aldoacevedo@protonmail.com>
2025-05-05 10:18:34 +0800

    Model loading fix 5

--- scripts/run_inference_linear.sh
+++ scripts/run_inference_linear.sh
@@ -6,7 +6,7 @@ CUDA_VISIBLE_DEVICES=0 accelerate launch \
     -m fastcomposer.inference \
     --mixed_precision=fp16 \
     --pretrained_model_name_or_path runwayml/stable-diffusion-v1-5 \
-    --finetuned_model_path model/stable-diffusion-v1-5/ffhq/fastcomposer-finetuned-linear_attn/checkpoint-*/model.safetensors \
+    --finetuned_model_path models/stable-diffusion-v1-5/ffhq/fastcomposer-finetuned-linear_attn/checkpoint-*/model.safetensors \
     --test_reference_folder data/${DEMO_NAME} \
     --test_caption "${CAPTION}" \
     --output_dir outputs/${DEMO_NAME} \
