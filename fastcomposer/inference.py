e92233412f7d59f02ea4349f44b960cea1f0651e
Sinono3 <aldoacevedo@protonmail.com>
2025-05-05 09:51:35 +0800

    Fix inference bug

--- fastcomposer/inference.py
+++ fastcomposer/inference.py
@@ -13,8 +13,7 @@ import torch
 import os
 from tqdm.auto import tqdm
 from fastcomposer.pipeline import (
-    stable_diffusion_call_with_references_delayed_conditioning,
-    stable_diffusion_call_with_references_delayed_conditioning_bench,
+    stable_diffusion_call_with_references_delayed_conditioning
 )
 import types
 import itertools
